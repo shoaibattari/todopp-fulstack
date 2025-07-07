842a27c055045f0550b63383724bcd2583b4d664
shoaib attari <shoaibattari83@gmail.com>
2025-07-07 12:19:38 +0500

    test push

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ import dotenv from "dotenv";
 import express from "express";
 import routes from "./routes/router.js";
 import mongoose, { connect } from "mongoose";
+import cors from "cors";
 const app = express();
 const port = 3001;
 dotenv.config();
 const dbURI = process.env.MONGODB_URI;
-
+app.use(cors());
 app.use(express.json()); // To parse JSON request bodies
 app.use(express.urlencoded({ extended: true }));
 
--- routes/router.js
+++ routes/router.js
@@ -6,9 +6,9 @@ const router = express.Router();
 
 router.post( "/addTodo", addTodo)   // add todo
 router.get( "/AllTodo", getAllTodo)  // get all todos
-router.get( "/AllTodo/:id", getTodo)   //single todo by id
-router.put( "/EditTodo/:id", editTodo)  // edit todo by id
-router.delete( "/DeleteTodo/:id", deleteTodo)   // delete todo by id
+router.get( "/AllTodo/:id", getTodo)   //get single todo by id
+router.put( "/EditTodo/:id", editTodo)  // edit single todo by id
+router.delete( "/DeleteTodo/:id", deleteTodo)   // delete single todo by id
 router.delete( "/DeleteAllTodos", deleteAllTodos)   // delete todo by all
 
 
